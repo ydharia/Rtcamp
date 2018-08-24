70b610bded0bcb1e232e213d47b0e22800627b0d
Yash Dharia <ydharia@gmail.com>
2018-08-24 23:50:20 +0530

    remove google key

--- application/controllers/Myfacebook.php
+++ application/controllers/Myfacebook.php
@@ -42,7 +42,7 @@ class myfacebook extends CI_Controller {
 		
 		$url_array = explode('?', 'https://'.$_SERVER ['HTTP_HOST'].$_SERVER['REQUEST_URI']);
 		$url = $url_array[0];		
-		$config = array("client_id"=>"962527986048-fko0boarhk09sclfu0igldkgj1po0p8d.apps.googleusercontent.com","client_secret"=>"By-esciDrlc7omI-_pVmHZag","redirect_uri"=>$url);		
+		$config = array("client_id"=>"CLIENT_ID","client_secret"=>"CLIENT_SECRET","redirect_uri"=>$url);		
 		$this->load->library('google_drive',$config);
 		
 		redirect(base_url()."myfacebook/albums");
